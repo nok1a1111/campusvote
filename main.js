fac40c51cb78e8ccf70e6227389b7fb96d852daa
nok1a1111 <urlcom1111@gmail.com>
2024-10-21 11:52:28 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.querySelectorAll('.vote-btn').forEach(button => {
     button.addEventListener('click', async () => {
         const city = button.classList[0].split('-')[0]
 
-        const response = await fetch(`/vote/${city}`, { method: 'POST' });
+        const response = await fetch(`/functions/vote/${city}`, { method: 'POST' });
         const newVotes = await response.text();
 
         document.getElementById(`${city}-votes`).innerText = newVotes;
@@ -13,7 +13,7 @@ document.querySelectorAll('.vote-btn').forEach(button => {
 async function updateVotes() {
     const cities = ['ikebukuro', 'tachikawa', 'machida', 'musashisakai'];
     for (const city of cities) {
-        const res=await fetch(`/vote/${city}`)
+        const res=await fetch(`/functions/vote/${city}`)
             .then(res=>{console.log(res);return res.text()})
     }
 }
