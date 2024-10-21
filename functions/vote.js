8f6b78a80a74e1fbb3960282bb1ff7e8dffcaae4
nok1a1111 <urlcom1111@gmail.com>
2024-10-21 14:35:32 +0900

    Update vote.js

--- functions/vote.js
+++ functions/vote.js
@@ -4,7 +4,8 @@ export async function onRequest(context) {
     if (request.url.endsWith('/vote')) {
         // Workersのエンドポイントにリダイレクト
         const newUrl = new URL(request.url);
-        newUrl.hostname = 'campus-vote.urlcom1111.workers.dev/'; // ここにWorkersのサブドメインを設定
+        newUrl.hostname = 'campus-vote.urlcom1111.workers.dev'; // サブドメインのみ
+        newUrl.pathname = '/vote'; // パスを適切に変更
         const newRequest = new Request(newUrl, request);
         return await fetch(newRequest);
     }
