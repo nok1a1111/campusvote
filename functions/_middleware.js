b19bd52eb6de838360b5e19ef72bd21267f6539e
nok1a1111 <urlcom1111@gmail.com>
2024-10-21 10:52:35 +0900

    Create _middleware.js

---
+++ functions/_middleware.js
@@ -0,0 +1,14 @@
+export async function onRequest(context) {
+    const { request } = context;
+
+    if (request.url.endsWith('/count')) {
+        // Workersのエンドポイントにリダイレクト
+        const newUrl = new URL(request.url);
+        newUrl.hostname = 'campus-vote.urlcom1111.workers.dev/'; // ここにWorkersのサブドメインを設定
+        const newRequest = new Request(newUrl, request);
+        return await fetch(newRequest);
+    }
+
+    // 他のリクエストはそのまま返す
+    return await context.next();
+}
