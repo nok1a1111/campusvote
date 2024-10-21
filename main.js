d72e153d02c7b5be29b9bcc78177f7ee78427e72
nok1a1111 <urlcom1111@gmail.com>
2024-10-21 15:18:27 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.querySelectorAll('.vote-btn').forEach(button => {
     button.addEventListener('click', async () => {
         const city = button.classList[0].split('-')[0]
 
-        const response = await fetch(`/vote/${city}`, { method: 'POST' });
+        const response = await fetch(`https://campus-vote.urlcom1111.workers.dev`, { method: 'POST' });
         console.log(response)
         const newVotes = await response.text();
         console.log(newVotes)
@@ -14,7 +14,7 @@ document.querySelectorAll('.vote-btn').forEach(button => {
 async function updateVotes() {
     const cities = ['ikebukuro', 'tachikawa', 'machida', 'musashisakai'];
     for (const city of cities) {
-        const res=await fetch(`/vote/${city}`)
+        const res=await fetch(`https://campus-vote.urlcom1111.workers.dev`)
             .then(res=>{console.log(res);return res.text()})
     }
 }
